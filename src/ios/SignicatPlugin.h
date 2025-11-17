96fd0193dc4c139a751e9cfbf454a5a1f578db77
apalmaPL <apalma@product-league.com>
2025-11-17 15:26:03 +0100

    Update SignicatPlugin.h

--- src/ios/SignicatPlugin.h
+++ src/ios/SignicatPlugin.h
@@ -1,7 +1,11 @@
 #import <Cordova/CDV.h>
+@class ConnectisSDKConfiguration;
 
 @interface SignicatPlugin : CDVPlugin
 
-- (void)startAuthentication:(CDVInvokedUrlCommand*)command;
+- (void)login:(CDVInvokedUrlCommand*)command;
+- (void)getAccessToken:(CDVInvokedUrlCommand*)command;
+- (void)enableDeviceAuth:(CDVInvokedUrlCommand*)command;
+- (void)disableDeviceAuth:(CDVInvokedUrlCommand*)command;
 
 @end
