ffea45172c8751103a1d7a84b797e40c62bf8759
Adriano Palma <neozarp@gmail.com>
2026-01-28 18:26:55 +0000

    ADASD

--- src/ios/AppDelegate.swift
+++ src/ios/AppDelegate.swift
@@ -6,11 +6,13 @@ class MyCustomAppDelegate: AppDelegate {
 
     override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool
     {
+        NSLog("APPDELEGATE!");
         // Get URL components from the incoming user activity
         return ConnectisSDK.continueLogin(userActivity: userActivity)
     }
     
     override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        NSLog("BADRUZ!");
         return true
     }
 
